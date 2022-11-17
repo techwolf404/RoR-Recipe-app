1204a9534a2ac545d2495a183e1782cb82045b80
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-11-17 11:30:13 +0300

    login, recipe lists etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+end
+
+devise_for :users
   resources :recipe_foods
   resources :recipes
   resources :foods
-  devise_for :users
+  resources :public_recipes
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "recipes#index"
+  root to: "public_recipes#index"
+
 end
